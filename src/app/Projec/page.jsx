ef93e915c68739bb931267aa80847270dff104c5
ulugbekAbdullayev <ulu23503@gmail.com>
2025-04-05 17:53:59 +0500

    200

--- src/app/Projec/page.jsx
+++ src/app/Projec/page.jsx
@@ -14,7 +14,7 @@ function page() {
                         Ant Design (Antd) – UI component library <br />
                         React Redux – State management <br />
                         Redux Toolkit – Simplified Redux logic</p> <br />
-                    <div className='flex gap-5 justify-center'>
+                    <div className='flex gap-5 justify-center pt-17'>
                         <button className='bg-gradient-to-r to-cyan-400 from-fuchsia-600 px-4 py-2 rounded-lg shadow transition-transform duration-300 hover:scale-110'><a href="https://milliy-taomlar-pi.vercel.app
 ">Live preview</a></button>
                         <button className='border px-4 py-2 rounded-lg transition-transform duration-300 hover:scale-110'><a href="https://github.com/UlugbekAbdulaev/milliy-taomlar.git">Check on GitHub</a></button>
@@ -32,7 +32,7 @@ function page() {
                         - Filter and view products by categories - <br />
                         It is a fully functional frontend interface for an e-commerce platform, designed to be responsive and user-friendly.</p><br />
 
-                    <div className='flex gap-5 justify-center'>
+                    <div className='flex gap-5 justify-center pt-17'>
                         <button className='bg-gradient-to-r to-cyan-400 from-fuchsia-600 px-4 py-2 rounded-lg shadow transition-transform duration-300 hover:scale-110'><a href="https://5-imtihon-two.vercel.app
 ">Live preview</a></button>
                         <button className='border px-4 py-2 rounded-lg transition-transform duration-300 hover:scale-110'><a href="https://github.com/UlugbekAbdulaev/5-imtihon.git">Check on GitHub</a></button>
@@ -41,10 +41,31 @@ function page() {
 
 
 
-                <div className='border border-cyan-300 rounded-2xl overflow-hidden text-center px-4 py-5'>bu yerda xam xuddi shundat projectlar bo'ladi</div>
+                <div className='border border-cyan-300 rounded-2xl overflow-hidden text-center px-4 py-5'>
+                <img src="/API.jpg" alt="" />
+                    <p className='text-2xl pt-3'>E-commerce API Next </p>
+                    <p className='pt-2 text-gray-400'>This project was developed using Next.js to enhance SEO capabilities. It functions as an online store, where products are fetched from a backend API and dynamically displayed on the frontend.
+
+The main goal of using Next.js was to leverage server-side rendering (SSR) for better visibility on search engines like Google. By serving pre-rendered content, the website can appear higher in search results when customers search for products online.
+
+Although initially tested with Vite, the project was transitioned to Next.js to take full advantage of SEO features and improve overall performance and indexing speed.</p><br />
+
+                    <div className='flex gap-5 justify-center'>
+                        <button className='bg-gradient-to-r to-cyan-400 from-fuchsia-600 px-4 py-2 rounded-lg shadow transition-transform duration-300 hover:scale-110'><a href="https://figma-dji-next.vercel.app
+
+">Live preview</a></button>
+                        <button className='border px-4 py-2 rounded-lg transition-transform duration-300 hover:scale-110'><a href="https://github.com/UlugbekAbdulaev/figma-DJI-next.git">Check on GitHub</a></button>
+                    </div>
+                </div>
             </div>
         </div>
     )
 }
 
 export default page
+
+
+
+
+ 
+
